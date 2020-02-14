fe0702d8876248e56a4b99a261ec95e6f8ee7b6b
jhasb <jbrito@ipca.pt>
2020-02-14 14:32:57 +0000

    Fix Flowers

--- HelloWorldFlowersMatplotlibTensorflow.py
+++ HelloWorldFlowersMatplotlibTensorflow.py
@@ -14,12 +14,16 @@ zip_file = tf.keras.utils.get_file(origin=_URL, fname="flower_photos.tgz", extra
 base_dir = os.path.join(os.path.dirname(zip_file), 'flower_photos')
 
 classes = ['roses', 'daisy', 'dandelion', 'sunflowers', 'tulips']
+dataset_split_percentage = 0.1 # 0.8 # percentage of images for training
+epochs = 5 #80
+batch_size = 100
+IMG_SHAPE = 150
 
 for cl in classes:
     img_path = os.path.join(base_dir, cl)
     images = glob.glob(img_path + '/*.jpg')
     print("{}: {} Images".format(cl, len(images)))
-    num_train = int(round(len(images)*0.8))
+    num_train = int(round(len(images) * dataset_split_percentage))
     train, val = images[:num_train], images[num_train:]
 
     for t in train:
@@ -36,19 +40,19 @@ for cl in classes:
         if not os.path.exists(os.path.join(base_dir, 'val', cl, bn)):
             shutil.move(v, os.path.join(base_dir, 'val', cl))
 
-print("training images:", round(len(images)*0.8))
+print("training images:", round(len(images)*dataset_split_percentage))
 train_dir = os.path.join(base_dir, 'train')
 val_dir = os.path.join(base_dir, 'val')
 
-batch_size = 100
-IMG_SHAPE = 150
+
 
 # This function will plot images in the form of a grid with 1 row and 5 columns where images are placed in each column.
 def plotImages(images_arr):
-    fig, axes = plt.subplots(1, 5, figsize=(20, 20))
+    fig, axes = plt.subplots(1, 5) #, figsize=(10, 10))
     axes = axes.flatten()
     for img, ax in zip(images_arr, axes):
         ax.imshow(img)
+
     plt.tight_layout()
     plt.show()
 
@@ -120,7 +124,7 @@ model.compile(
     loss='sparse_categorical_crossentropy',
     metrics=['accuracy'])
 
-epochs = 80
+
 history = model.fit_generator(
     train_data_gen,
     steps_per_epoch=int(np.ceil(train_data_gen.n / float(batch_size))),
@@ -128,8 +132,8 @@ history = model.fit_generator(
     validation_data=val_data_gen,
     validation_steps=int(np.ceil(val_data_gen.n / float(batch_size))) )
 
-acc = history.history['acc']
-val_acc = history.history['val_acc']
+acc = history.history['accuracy']
+val_acc = history.history['val_accuracy']
 loss = history.history['loss']
 val_loss = history.history['val_loss']
 epochs_range = range(epochs)
