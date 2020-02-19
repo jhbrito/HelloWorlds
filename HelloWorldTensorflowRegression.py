c94838fc3277cd9c2723a441e6c0eb9238c7951e
jhasb <jbrito@ipca.pt>
2020-02-19 13:01:17 +0000

    Added regression example

---
+++ HelloWorldTensorflowRegression.py
@@ -0,0 +1,52 @@
+# regression to convert from Celsius to Fahrenheit
+# C ×1.8 + 32 = F
+
+import tensorflow as tf
+import numpy as np
+import matplotlib.pyplot as plt
+
+celsius_q    = np.array([-40, -10,  0,  8, 15, 22,  38],  dtype=float)
+fahrenheit_a = np.array([-40,  14, 32, 46, 59, 72, 100],  dtype=float)
+
+for i,c in enumerate(celsius_q):
+    print("{} degrees Celsius = {} degrees Fahrenheit".format(c, fahrenheit_a[i]))
+
+l0 = tf.keras.layers.Dense(units=1, input_shape=[1])
+model = tf.keras.Sequential([l0])
+# or
+# model = tf.keras.Sequential([
+#     tf.keras.layers.Dense(units=1, input_shape=[1])
+#     ])
+
+model.compile(
+    loss='mean_squared_error',
+    optimizer=tf.keras.optimizers.Adam(0.1)
+)
+
+history = model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+print("Finished training the model")
+
+plt.plot(history.history['loss'])
+plt.xlabel('Epoch Number')
+plt.ylabel("Loss Magnitude")
+
+
+print("100 C =",model.predict([100.0]))
+
+l0_weights = l0.get_weights()
+print("Layer variables: {}".format(l0_weights))
+
+l0 = tf.keras.layers.Dense(units=4, input_shape=[1])
+l1 = tf.keras.layers.Dense(units=4)
+l2 = tf.keras.layers.Dense(units=1)
+model = tf.keras.Sequential([l0, l1, l2])
+model.compile(
+    loss='mean_squared_error',
+    optimizer=tf.keras.optimizers.Adam(0.1))
+model.fit(celsius_q, fahrenheit_a, epochs=500, verbose=False)
+print("Finished training the model")
+print(model.predict([100.0]))
+print("Model predicts that 100 degrees Celsius is: {} degrees Fahrenheit".format(model.predict([100.0])))
+print("These are the l0 variables: {}".format(l0.get_weights()))
+print("These are the l1 variables: {}".format(l1.get_weights()))
+print("These are the l2 variables: {}".format(l2.get_weights()))
