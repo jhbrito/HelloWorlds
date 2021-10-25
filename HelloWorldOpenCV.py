6eed62be399fe7fd76b477489cc29fd3149b72b5
jhasb <jbrito@ipca.pt>
2021-10-25 01:10:32 +0100

    small fixes added qr code example

--- HelloWorldOpenCV.py
+++ HelloWorldOpenCV.py
@@ -146,3 +146,31 @@ image_faces = image.copy()
 for (x, y, w, h) in faces:
     cv2.rectangle(image_faces, (x, y), (x + w, y + h), (0, 255, 0), 2)
 viewImage(image_faces, "Lena with {} faces detected".format(len(faces)))
+
+
+def display_box(im, bbox):
+    n_boxes = len(bbox)
+    for j_box in range(n_boxes):
+        for j in range(4):
+            cv2.line(im,
+                     (int(bbox[j_box][j][0]), int(bbox[j_box][j][1])),
+                     (int(bbox[j_box][(j + 1) % 4][0]), int(bbox[j_box][(j + 1) % 4][1])),
+                     (255, 0, 0), 3)
+        # Display results
+    cv2.imshow("Results", im)
+
+
+inputImage = cv2.imread("qrcode.jpg")
+qrDecoder = cv2.QRCodeDetector()
+data, bbox, rectifiedImage = qrDecoder.detectAndDecode(inputImage)
+if len(data) > 0:
+    print("Decoded Data : {}".format(data))
+    display_box(inputImage, bbox)
+    rectifiedImage = np.uint8(rectifiedImage)
+    cv2.imshow("Rectified QRCode", rectifiedImage)
+else:
+    print("QR Code not detected")
+    cv2.imshow("Results", inputImage)
+
+cv2.waitKey(0)
+cv2.destroyAllWindows()
